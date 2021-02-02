4d01494f05acc76af00fbfc48d18be91ceb243ef
华南小无 <HuananXiaoWU@Outlook.com>
2021-02-02 16:53:17 +0800

    首次添加文件

---
+++ ClassAndInstance/ClassAndInstance/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClassAndInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form MyFrom;//声明一个引用变量
+            MyFrom = new Form();//实例化一个窗体对象并赋值引用变量
+            MyFrom.Text = "Hello";//设置窗体对象的表头文本
+            MyFrom.ShowDialog();//打开窗体
+        }
+    }
+}
